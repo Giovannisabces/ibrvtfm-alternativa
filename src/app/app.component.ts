d7c871fea793e8152be32900653de2f7df498e16
Giovanni Sabogal Cespedes <giovannisc22@gmail.com>
2024-05-22 18:01:38 +0200

    Delete unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
